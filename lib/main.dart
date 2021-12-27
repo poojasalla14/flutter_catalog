a8ea9fe4b242e6c1ae6fd4e25b14e10518d87e2c
Pushpanjali Salla <poojasalla14@gmail.com>
2021-12-27 18:49:55 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
